ccdaf60fc287e9225922dee94cd754fa2f25d6cc
Shatinmg <lotte2027@naver.com>
2021-11-25 04:01:07 +0900

    Potalmade

--- Assets/Scripts/ObjectBase.cs
+++ Assets/Scripts/ObjectBase.cs
@@ -1,9 +1,12 @@
 
 using UnityEngine;
 
+
 public class ObjectBase
 {
     public float speed;
     public Vector3 vector;
     public Animator animator;
+    public Transform targettransform;
+    public bool outmingame=false;
 }
