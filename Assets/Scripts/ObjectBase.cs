c99d4d8d4adea2ce89992cb64d4ac625b0597a7f
Shatinmg <lotte2027@naver.com>
2021-11-24 22:09:59 +0900

    PlayerAnimator

--- Assets/Scripts/ObjectBase.cs
+++ Assets/Scripts/ObjectBase.cs
@@ -1,9 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class ObjectBase
 {
     public float speed;
     public Vector3 vector;
+    public Animator animator;
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,33 +9,42 @@ public class Player : MonoBehaviour
     void Start()
     {
         mOb = new ObjectBase();
+        mOb.animator = GetComponent<Animator>();
         mOb.speed = 10.0f;
     }
 
     public void Move()
     {
         mOb.vector = Vector3.zero;
-        if (Input.GetAxisRaw("Horizontal") != 0)
-        {
-            mOb.vector = (Input.GetAxisRaw("Horizontal") > 0) ? Vector3.right : Vector3.left;
-            
-          
+        
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                mOb.vector = (Input.GetAxisRaw("Horizontal") > 0) ? Vector3.right : Vector3.left;
+                this.transform.localScale = (Input.GetAxisRaw("Horizontal") > 0) ? new Vector3(1, this.transform.localScale.y, this.transform.localScale.z) : new Vector3(-1, this.transform.localScale.y, this.transform.localScale.z);
 
-           
-        }
-        if (Input.GetAxisRaw("Vertical") != 0)
-        {
-            mOb.vector = (Input.GetAxisRaw("Vertical") > 0) ? Vector3.up : Vector3.down;
 
-            
-        }
-        Debug.Log(transform.position);
+
+
+            }
+            if (Input.GetAxisRaw("Vertical") != 0)
+            {
+                mOb.vector = (Input.GetAxisRaw("Vertical") > 0) ? Vector3.up : Vector3.down;
+
+
+            }
+        mOb.animator.SetBool("Move",((Input.GetAxisRaw("Horizontal") != 0)|| (Input.GetAxisRaw("Vertical") != 0))? true:false);
+        
+        
         transform.position += mOb.vector * mOb.speed * Time.deltaTime;
     }
     // Update is called once per frame
     void Update()
     {
-        Move(); 
+        
+    }
+    private void FixedUpdate()
+    {
+        Move();
     }
 
 }
