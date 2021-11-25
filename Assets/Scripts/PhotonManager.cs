5bd8818f7f918db1c8bf3e7a8e82c33e1d8d25d5
tyrosine1153 <tyrosine1153@gmail.com>
2021-11-25 17:50:11 +0900

    Add Photon Default Scripts

---
+++ Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonManager : PunCallbacksSingleton<PhotonManager>
+{
+
+    public void ConnectToPhoton()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void JoinRoomMap()
+    {
+        PhotonNetwork.JoinRandomOrCreateRoom();
+    }
+
+    public void LeaveRoomMap()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+    
+    public void Disconnect()
+    {
+        PhotonNetwork.Disconnect();
+    }
+
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        SceneManagerEx.Instance.LoadScene(SceneType.Lobby);
+        // Do Something
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel((int) SceneType.RoomMap);
+        // Do Something
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message) => Debug.LogError("방만들기실패");
+    public override void OnJoinRoomFailed(short returnCode, string message) => Debug.LogError("방참가실패");
+    public override void OnJoinRandomFailed(short returnCode, string message) => Debug.LogError("방랜덤참가실패");
+
+    public override void OnLeftRoom()
+    {
+        SceneManagerEx.Instance.LoadScene(SceneType.Lobby);
+        // Do Something
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("연결끊김");
+        // Do Something
+    }
+
+}
